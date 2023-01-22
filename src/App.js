5f399323198ee52aefd07e7feb6a5ae82229cd96
minsoo <als494@naver.com>
2023-01-22 22:19:38 +0900

    feat: header 세팅

--- src/App.js
+++ src/App.js
@@ -5,20 +5,10 @@ import New from "./pages/New";
 import Edit from "./pages/Edit";
 import Diary from "./pages/Diary";
 
-// components
-import MyButton from "./components/MyButton";
-
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
-        <h2>처음 시작</h2>
-
-        <MyButton text={"버튼"} onClick={() => alert("버튼클릭")} type={"positive"} />
-        <MyButton text={"버튼"} onClick={() => alert("버튼클릭")} />
-        <MyButton text={"버튼"} onClick={() => alert("버튼클릭")} type={"negative"} />
-        <MyButton text={"버튼"} onClick={() => alert("버튼클릭")} type={"ascasc"} />
-
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/new" element={<New />} />
