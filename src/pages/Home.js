b8122d0ac1698a3f6a4e0627c22f06670d866249
minsoo <als494@naver.com>
2023-01-23 01:01:27 +0900

    feat: Home의 Header

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,30 @@
+import { useState } from "react";
+import MyButton from "../components/MyButton";
+import MyHeader from "../components/MyHeader";
+
 const Home = () => {
+  const [curDate, setCurDate] = useState(new Date());
+
+  const headText = `${curDate.getFullYear()}년 ${curDate.getMonth() + 1}월`;
+
+  const increaseMonth = () => {
+    setCurDate(new Date(curDate.getFullYear(), curDate.getMonth() + 1, curDate.getDate()));
+  };
+
+  const decreaseMonth = () => {
+    setCurDate(new Date(curDate.getFullYear(), curDate.getMonth() - 1, curDate.getDate()));
+  };
+
   return (
     <div>
+      <MyHeader
+        headText={headText}
+        leftChild={<MyButton text={"<"} onClick={decreaseMonth} />}
+        rightChild={<MyButton text={">"} onClick={increaseMonth} />}
+      />
       <h1>Home</h1>
     </div>
-  )
-}
+  );
+};
 
 export default Home;
