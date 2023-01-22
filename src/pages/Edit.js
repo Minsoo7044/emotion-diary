802ae6d4d4ef06118d409cac2a37650b44c3db0f
minsoo <als494@naver.com>
2023-01-22 21:05:42 +0900

    feat: Query String -> useSearchParams

--- src/pages/Edit.js
+++ src/pages/Edit.js
@@ -1,9 +1,20 @@
+import { useSearchParams } from "react-router-dom";
+
 const Edit = () => {
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const id = searchParams.get("id");
+  console.log(id);
+
+  const mode = searchParams.get("mode");
+  console.log(mode);
+
   return (
     <div>
       <h1>Edit</h1>
+      <button onClick={() => setSearchParams({ who: "minsoo" })}>쿼리스트링 바꾸기</button>
     </div>
-  )
-}
+  );
+};
 
 export default Edit;
