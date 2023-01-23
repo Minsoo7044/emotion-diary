eedff2d20e37589d451d79c423c16e8de1dd56a2
minsoo <als494@naver.com>
2023-01-24 02:16:43 +0900

    feat: new 페이지 header 부분

--- src/pages/New.js
+++ src/pages/New.js
@@ -1,9 +1,15 @@
+import { useNavigate } from "react-router-dom";
+import MyHeader from "../components/MyHeader";
+import MyButton from "../components/MyButton";
+
 const New = () => {
+  const navigate = useNavigate();
+
   return (
     <div>
-      <h1>New</h1>
+      <MyHeader headText={"새 일기 쓰기"} leftChild={<MyButton text={"< 뒤로가기"} onClick={() => navigate(-1)} />} />
     </div>
-  )
-}
+  );
+};
 
 export default New;
