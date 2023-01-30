a5d0ee4d302f424983c9831a6ee4b609f12f0840
minsoo <als494@naver.com>
2023-01-30 15:18:26 +0900

    fix: 일기 리스트가 비어있을 때 오류 고침

--- src/App.js
+++ src/App.js
@@ -40,10 +40,10 @@ function App() {
   useEffect(() => {
     const localData = localStorage.getItem("diary");
     if (!localData) return;
-
     const diaryList = JSON.parse(localData).sort((a, b) => parseInt(b.id) - parseInt(a.id));
-    dataId.current = parseInt(diaryList[0].id) + 1;
 
+    if (diaryList.length < 1) return;
+    dataId.current = parseInt(diaryList[0].id) + 1;
     dispatch({ type: "INIT", data: diaryList });
   }, []);
 
